d09ea52063ee324ec0b866492a64efa34d53b189
Semenov Petr <under_karter49@icloud.com>
2022-06-05 21:55:46 +0300

    Initial Commit

---
+++ iOS6-HW13-Setting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW13-Setting
+//
+//  Created by temp user on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
